837035d74a757a1943eedc8c3abe7ea9a960ac43
user1 <rkr15803@gmail.com>
2026-08-13 10:38:24 +0530

    added a few functions

--- app.js
+++ app.js
@@ -18,5 +18,22 @@ export default function myApp(){
 }
 
 export function sayHi(user){
-    alert(`Hi!`)
+    alert(`Hi! ${user}`);
 }
+
+const add = (a, b)=>{
+    const result=a+b;
+    return result;
+}
+
+function greet(user){
+    return `Hello ${user}!`;
+}
+
+const greet=name=>{
+    return `Hi  there`;
+}
+
+const funcAdd=(a, b)=>a+b;
+
+const squareNum=a=>a*a;
