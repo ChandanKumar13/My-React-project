e5bc6ef26e83d9773d0a2a02377e01fe05761670
user1 <rkr15803@gmail.com>
2026-08-13 10:48:03 +0530

    some funcs

--- app.js
+++ app.js
@@ -37,3 +37,13 @@ const greet=name=>{
 const funcAdd=(a, b)=>a+b;
 
 const squareNum=a=>a*a;
+
+const nums=[1,2,3,4,5];
+const doubled=nums.map(n=>n*2);
+
+const evens=nums.filter(n=>n%2===0);
+
+const marks=[10,20,30,50];
+const filteredMarks=marks.filter(n=>n>20);
+console.log(filteredMarks);
+
